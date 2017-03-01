dd937914e0e0dcae908c8d9784f020e99cc70d72
Axel Naumann <Axel.Naumann@cern.ch>
2017-03-01 20:32:34 +0100

    Replace Jira URLs with markdown links.

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: jira
+# about: Replaces links to Jira by issue number and an state icon
+# version: 0.0.1
+# authors: Axel Naumann axel@cern.ch
+
+#register_asset "javascripts/jira-plugin-replace.js", :server_side
